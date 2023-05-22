454851d3a9b92c29cb0d38d729889f5278e01953
Scztz <134317546+Scztz@users.noreply.github.com>
2023-05-22 15:12:17 -0300

    opção de saida
    incluso no sistema a opçao de saída do usuário.

--- cartorio.paulo (1).cpp
+++ cartorio.paulo (1).cpp
@@ -128,6 +128,7 @@ int main()
 	 printf("\t1 - Registrar nomes\n");
   	 printf("\t2 - Consultar nomes\n");
 	 printf("\t3 - Deletar nomes\n\n"); 
+	 printf("\t4 - sair do sistema\n\n");
 	 printf("Opção ;");//fim do menu
    
      scanf("%d", &opcao); //armamzenando a escolha do usuário
@@ -145,10 +146,18 @@ int main()
 	    consulta();
 	    break;
 	
-        case 3 :
+        case 3:
 		deletar();	
    	    break;
    	    
+   	    
+   	    case 4:
+   	    printf("Obrigado por utilizar o sistema!\n");
+		return 0;
+		break;
+		
+	 	
+   	    	
    	    
    	    
    	    default:
